a9a19ef631519bf2318bc9066bafd6ff76112af1
Aleksi Ahtiainen <aleksi.ahtiainen@iki.fi>
2017-07-26 12:26:44 +0300

    Compositions are now also added to the result data

--- test1/server.js
+++ test1/server.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var request = require('request');
+//var request = require('request');
+var request = require('request-promise-native');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
 // Config
 var port = process.env.PORT || 3000;
+var trainsApiUrl = 'https://rata.digitraffic.fi/api/v1/live-trains';
+var compositionApiUrl = 'https://rata.digitraffic.fi/api/v1/compositions';
 
-var trainsUrl = 'https://rata.digitraffic.fi/api/v1/live-trains?station=';
-var compositionsUrl = 'https://rata.digitraffic.fi/api/v1/compositions/<train>?departure_data=<date>';
+//var trainsUrl = 'https://rata.digitraffic.fi/api/v1/live-trains?station=';
+//var compositionsUrl = 'https://rata.digitraffic.fi/api/v1/compositions/<train>?departure_data=<date>';
 
 var app = express();
 
@@ -17,15 +21,20 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var trainComposition = function(paramObject, reqApi, resApi) {
+  // TODO: parameter validation and error handling:
+
+  // TODO: Clean up the code to use promises more cleanly, e.g.
+  // the data returned from the 2 APIs can be combined together
+  // more cleanly by the trainNumber as an id.
+
   var stationShortCode  = paramObject.stationShortCode;
+
   var trainNumbers = paramObject.trainNumber.split(',').map(Number);
 
   console.log('Query station:' + stationShortCode);
   console.log('Query train numbers:' + trainNumbers);
 
-  // TODO: parameter validation and error handling
-
-  var trainsUrl = 'https://rata.digitraffic.fi/api/v1/live-trains?station=' + stationShortCode;
+  var trainsUrl = trainsApiUrl + '?station=' + stationShortCode;
 
   request.get({
       url: trainsUrl,
@@ -40,29 +49,69 @@ var trainComposition = function(paramObject, reqApi, resApi) {
         // data is already parsed as JSON
 
         // filter the list by the trainNumbers from the query
-        data = data.filter(function (trainData) {
+        data = data.filter((trainData) => {
           return trainNumbers.includes(trainData.trainNumber); 
         });
+
+        // TODO: sort the data to the same order as the queried numbers? This
+        // was not specified as a requirement, though
 
         // TODO: Get the compositions
         // Do this using multiple asynchronous requests? On the other hand,
         // it might overflow the compositions API, so doing this in sequence might
         // actually be better.
-        
-        var resultData = data.map(function (train) {
-          return {
-            departureDate: train.departureDate,
-            trainName: train.trainType + train.trainNumber,
 
-            // The time table rows are ordered by the train's route
-            departureStation: train.timeTableRows[0].stationShortCode, 
-            destinationStation: train.timeTableRows[train.timeTableRows.length - 1].stationShortCode,
-          }
+        var compositionUrls = data.map((train) => {
+          // It is probably safer to use the date returned from the live-trains API (vs current date), since it 
+          // probably works better with trains that travel across midnight
+
+          return compositionApiUrl + '/' + train.trainNumber + '?departure_date=' + train.departureDate;
         });
 
-        console.log('Query response:' + resultData);
+        console.log('compositionUrls:' + compositionUrls);
 
-        resApi.json(resultData);
+        const compositionPromises = compositionUrls.map((url) => {
+          return request.get({
+              url: url,
+              json: true,
+              headers: {'User-Agent': 'request'}
+            });
+        });
+
+        Promise.all(compositionPromises).then((compositionData) => {
+          console.log('Composition query results:' + compositionData);
+
+          // Create composition data
+          var resultCompositionData = compositionData.map((composition) => { 
+            return composition.journeySections.map((journeySection) => {
+              return {
+                from: journeySection.beginTimeTableRow.stationShortCode,
+                to: journeySection.endTimeTableRow.stationShortCode,
+                wagons: journeySection.wagons,
+                locomotives: journeySection.locomotives
+              };
+            });
+          });
+
+          // Create the result object 
+          var resultData = data.map((train, index) => {
+            return {
+              departureDate: train.departureDate,
+              trainName: train.trainType + train.trainNumber,
+
+              // The time table rows are ordered by the train's route
+              departureStation: train.timeTableRows[0].stationShortCode, 
+              destinationStation: train.timeTableRows[train.timeTableRows.length - 1].stationShortCode,
+              composition: resultCompositionData[index]
+            }
+          });
+
+          console.log('Query response:' + resultData);
+
+          resApi.json(resultData);
+
+        });
+
       }
   });
 }
